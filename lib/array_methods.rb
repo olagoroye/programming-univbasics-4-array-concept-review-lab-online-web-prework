8d0add87093fe89738b68edbe343cf11d5003ff2
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-03 04:36:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -25,10 +25,11 @@ end
 
 def find_min_value(array)
 x = array[0]
-array.length.times do |index|
+array.length.times do {|index|
   if array[index] < x     
     x = array[index]
   end
-
+}
+x
 
 end
