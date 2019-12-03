4ed15336fe46af7f221a08253d24245b79ea4c32
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-03 04:47:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,4 +10,19 @@ end
   end
 end
 
+def find_max_value(array)
+  
+  counter = 0 
+  while counter < array.length do 
+    if array[index] > counter
+      counter = array[index]
+      return counter
+    end
+    
+    counter += 1 
+  
+end
+
+end
+
 
