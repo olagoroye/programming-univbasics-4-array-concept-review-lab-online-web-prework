456695d8abdf15cfa4588442df80cafe7d8d2e44
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-03 02:35:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,16 @@
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+index = 0 
+ array.length.times
+ if array[index] == value_to_find do 
+   puts index
+   index += 1
+   
+ end
+     return array[index]
+
+
 end
 
-def find_max_value(array)
-  # Add your solution here
-end
 
-def find_min_value(array)
-  # Add your solution here
-end
+
