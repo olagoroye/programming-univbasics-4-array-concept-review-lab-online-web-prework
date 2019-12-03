4fd0144e67f62104a9e160671521e8232bc3fcfb
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-03 03:24:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,11 @@
 
 def find_element_index(array, value_to_find)
   counter = 0
-  while counter < array.length  
+  while counter < array.length do
+ if  array[counter] == value_to_find
+  return array[index]
+end
+    counter += 1
 
   end
 end
